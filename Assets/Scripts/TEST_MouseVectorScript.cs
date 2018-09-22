ec4ce57cf1969b299a43f5002024eaa2c6bea91f
IanClayman <Ian.clayman1@gmail.com>
2018-09-22 18:12:44 -0400

    UI reconfigure

--- Assets/Scripts/TEST_MouseVectorScript.cs
+++ Assets/Scripts/TEST_MouseVectorScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Ara;
+using TMPro;
 
 [RequireComponent(typeof(AraTrail))]
 public class TEST_MouseVectorScript : MonoBehaviour {
@@ -9,12 +10,17 @@ public class TEST_MouseVectorScript : MonoBehaviour {
     [Range(90, 180)]
     public float angleThreshhold = 90;
 
+    public TextMeshProUGUI messageLog;
+
+    [SerializeField]
     private Vector3[] mousePositions = new Vector3[3];
 
     private AraTrail trail;
+    private ParticleSystem particle;
 	// Use this for initialization
 	void Start () {
         trail = gameObject.GetComponent(typeof(AraTrail)) as AraTrail;
+        particle = gameObject.GetComponent(typeof(ParticleSystem)) as ParticleSystem;
         trail.emit = false;
         //trail.emitting = false;
 	}
@@ -32,11 +38,15 @@ public class TEST_MouseVectorScript : MonoBehaviour {
                 mousePositions[2] = mousePositions[1];
                 mousePositions[1] = mousePositions[0];
                 mousePositions[0] = Input.mousePosition;
+            }
 
-                lastVelocity = mousePositions[2] - mousePositions[1];
-                thisVelocity = mousePositions[1] - mousePositions[0];
+            lastVelocity = mousePositions[2] - mousePositions[1];
+            thisVelocity = mousePositions[1] - mousePositions[0];
 
-                Plane objPlane = new Plane(Camera.main.transform.forward * -1, Camera.main.transform.position + Camera.main.transform.forward * (5 + Random.Range(0.1f, 0.51f)));
+            if (Vector3.Angle(lastVelocity, thisVelocity) <= angleThreshhold)
+            {
+                //Plane objPlane = new Plane(Camera.main.transform.forward * -1, this.transform.position);
+                Plane objPlane = new Plane(Camera.main.transform.forward * -1, Camera.main.transform.position + Camera.main.transform.forward * 5);
                 Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
                 float rayDistance;
                 if (objPlane.Raycast(mRay, out rayDistance))
@@ -45,25 +55,14 @@ public class TEST_MouseVectorScript : MonoBehaviour {
                     trail.emit = true;
                 }
             }
-            /*
-                if (Vector3.Angle(lastVelocity, thisVelocity) <= angleThreshhold) {
-                //Plane objPlane = new Plane(Camera.main.transform.forward * -1, this.transform.position);
-                Plane objPlane = new Plane(Camera.main.transform.forward * -1, Camera.main.transform.position + Camera.main.transform.forward * (5 + Random.Range(0.1f, 0.51f)));
-                Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                float rayDistance;
-                if (objPlane.Raycast(mRay, out rayDistance)) {
-                    this.transform.position = mRay.GetPoint(rayDistance);
-                    trail.emit = true;
-                }
-            } else {
-                Debug.Log("Angle btw last frame and this is " + Vector3.Angle(lastVelocity, thisVelocity) + " degrees!");
+            else
+            {
+                messageLog.text = ("Angle btw last frame and this is " + Vector3.Angle(lastVelocity, thisVelocity) + " degrees!");
                 trail.emit = false;
             }
-            */
         } else {
             trail.emit = false;
-        }
-            
+        }         
 	}
 
 }
